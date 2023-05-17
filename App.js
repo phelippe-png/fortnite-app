137c26d6ec2709b05c2cbbd4161ca7064f56dc09
Phelippe Vilas Boas <phelippe.gabriel.7@gmail.com>
2023-05-16 22:27:00 -0300

    commit 16/05/2023

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
 
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image, Pressable, ActivityIndicator } from 'react-native';
 import apiHttp from './API/api.js';
 import { useFonts, Dangrek_400Regular } from '@expo-google-fonts/dangrek';
-import ScrollViewItems from './src/ScrollViewItems.js';
+import DescriptionItem from './src/DescriptionItem.js';
 
 export default function App() {
   const [apiItems, setApiItems] = useState();
   const [items, setItems] = useState();
+  const [stateDescriptionItem, setStateDescriptionItem] = useState(false);
+  const [selectedItem, setSelectedItem] = useState({});
+  const [loading, setLoading] = useState(true);
   const [fontLoaded] = useFonts({
     Dangrek_400Regular,
   });
 
   useEffect(() => {
-    apiHttp.get().then((response) => setApiItems(response.data))
-      .catch((err) => {
-        console.error("Erro: " + err);
-      });
+    apiHttp.get().then((response) => {
+      setApiItems(response.data);
+      setLoading(false);
+    }).catch((err) => {
+      console.error("Erro: " + err);
+    });
   }, []);
 
   if (!apiItems) {
@@ -27,24 +32,56 @@ export default function App() {
     return null;
   }
 
+  const loadItems = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 0);
+  }
+
+  const ScrollViewItems = props => {
+    return (
+      <ScrollView>
+        {loading ?
+          <ActivityIndicator size={90} color={'#6FC1E5'} animating={true} style={styles.loadIndicator} /> :
+          <View style={styles.containerItems}>
+            {props.items?.map((item) => (
+              <Pressable key={item.id} style={styles.items} onPress={() => { setStateDescriptionItem(true); setSelectedItem(item) }}>
+                <Image source={{ uri: item.images.icon }} style={{ width: 100, height: 110, }} />
+                <Text style={styles.textItems}>{item.name}</Text>
+                <View style={{ justifyContent: 'center', flexDirection: 'row' }}>
+                  <Image source={{ uri: 'https://image.fnbr.co/price/icon_vbucks.png' }} style={{ width: 20, height: 20 }} />
+                  <Text style={styles.textItems}>{item.price}</Text>
+                </View>
+              </Pressable>
+            ))}
+          </View>
+        }
+      </ScrollView>
+    )
+  }
+
   return (
     <View style={styles.container}>
+      {!stateDescriptionItem ? (
+        <View style={{height: '100%'}}>
+          <View style={styles.panelTop}>
+            <Text style={{ fontSize: 50, fontWeight: '600', color: 'white', fontFamily: 'Dangrek_400Regular', }}>Loja Fortnite</Text>
+            <View style={{ flexDirection: 'row' }}>
+              <TouchableOpacity style={styles.buttonsTop} onPress={() => { setItems(apiItems?.data.featured); loadItems() }}>
+                <Text style={styles.textButtons}>DESTAQUES</Text>
+              </TouchableOpacity >
+             <TouchableOpacity style={styles.buttonsTop} onPress={() => { setItems(apiItems?.data.daily); loadItems() }}>
+                <Text style={styles.textButtons}>DIARIA</Text>
+              </TouchableOpacity >
+            </View>
+          </View>
 
-      <View style={styles.panelTop}>
-        <Text style={{ fontSize: 50, fontWeight: '600', color: 'white', fontFamily: 'Dangrek_400Regular', }}>Loja Fortnite</Text>
-        <View style={{ flexDirection: 'row' }}>
-          <TouchableOpacity style={styles.buttonsTop} onPress={() => { setItems(apiItems?.data.featured) }}>
-            <Text style={styles.textButtons}>DESTAQUES</Text>
-          </TouchableOpacity >
-          <TouchableOpacity style={styles.buttonsTop} onPress={() => { setItems(apiItems?.data.daily) }}>
-            <Text style={styles.textButtons}>DIARIA</Text>
-          </TouchableOpacity >
+          {!items ? (<ScrollViewItems items={apiItems?.data.featured}></ScrollViewItems>) :
+            (<ScrollViewItems items={items}></ScrollViewItems>)
+          }
         </View>
-      </View>
-
-      {!items ? (<ScrollViewItems items={apiItems?.data.featured}></ScrollViewItems>) :
-                (<ScrollViewItems items={items}></ScrollViewItems>)
-      }
+      ) : <DescriptionItem item={selectedItem} close={setStateDescriptionItem}></DescriptionItem>}
 
     </View>
   );
@@ -96,5 +133,12 @@ const styles = StyleSheet.create({
     fontSize: 15,
     textAlign: 'center',
     color: 'white'
+  },
+
+  loadIndicator: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 80
   }
 });
